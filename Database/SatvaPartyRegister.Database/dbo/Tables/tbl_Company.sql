6010601b74aae2d2301b71a38e816206a0691e20
Ravi Pipaliya <Ravi Pipaliya@DESKTOP-TPT38UD>
2018-08-29 23:42:23 +0530

    Added Database Project

---
+++ Database/SatvaPartyRegister.Database/dbo/Tables/tbl_Company.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tbl_Company] (
+    [TenantId]   NVARCHAR (128) NOT NULL,
+    [Name]       NVARCHAR (512) NOT NULL,
+    [IsActive]   BIT            CONSTRAINT [DF_tbl_Company_IsActive] DEFAULT ((1)) NOT NULL,
+    [CreatedBy]  INT            NULL,
+    [CreatedOn]  DATETIME       CONSTRAINT [DF_tbl_Company_CreatedOn] DEFAULT (getdate()) NULL,
+    [ModifiedBy] INT            NULL,
+    [ModifiedOn] DATETIME       NULL,
+    [RowVersion] ROWVERSION     NOT NULL,
+    [IsDeleted]  BIT            CONSTRAINT [DF_tbl_Company_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_tbl_Company] PRIMARY KEY CLUSTERED ([TenantId] ASC)
+);
+
---
+++ Database/SatvaPartyRegister.Database/dbo/Tables/tbl_FinancialYear.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tbl_FinancialYear] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (50)  NOT NULL,
+    [TenantId]   NVARCHAR (512) NOT NULL,
+    [IsActive]   BIT            CONSTRAINT [DF_tbl_FinancialYear_IsActive] DEFAULT ((1)) NOT NULL,
+    [CreatedBy]  INT            NULL,
+    [CreatedOn]  DATETIME       CONSTRAINT [DF_tbl_FinancialYear_CreatedOn] DEFAULT (getdate()) NULL,
+    [ModifiedBy] INT            NULL,
+    [ModifiedOn] DATETIME       NULL,
+    [RowVersion] ROWVERSION     NOT NULL,
+    [IsDeleted]  BIT            CONSTRAINT [DF_tbl_FinancialYear_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_tbl_FinancialYear] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
