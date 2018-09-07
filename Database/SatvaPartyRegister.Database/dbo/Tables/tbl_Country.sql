6e32bfc57f3552258b7b2a1c6edcca96a9cd9e55
Ravi Pipaliya <Ravi Pipaliya@DESKTOP-TPT38UD>
2018-09-08 01:34:38 +0530

    Modified tables and added class

--- Database/SatvaPartyRegister.Database/dbo/Tables/tbl_Country.sql
+++ Database/SatvaPartyRegister.Database/dbo/Tables/tbl_Country.sql
@@ -1,15 +1,19 @@
 ﻿CREATE TABLE [dbo].[tbl_Country] (
-    [Id]         INT           NOT NULL,
-    [Code]       NVARCHAR (50) NOT NULL,
-    [Name]       VARCHAR (50)  NOT NULL,
-    [ShortCode]  VARCHAR (10)  NULL,
-    [CreatedOn]  DATETIME      CONSTRAINT [DF_tbl_Country_CreatedOn] DEFAULT (getdate()) NOT NULL,
-    [CreatedBy]  INT           NULL,
-    [ModifiedOn] DATETIME      NULL,
-    [ModifiedBy] INT           NULL,
-    [RowVersion] ROWVERSION    NULL,
-    CONSTRAINT [PK_tbl_Country] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Id]         INT            NOT NULL,
+    [Code]       NVARCHAR (50)  NOT NULL,
+    [Name]       VARCHAR (50)   NOT NULL,
+    [TenantId]   NVARCHAR (128) NOT NULL,
+    [ShortCode]  VARCHAR (10)   NULL,
+    [CreatedOn]  DATETIME       CONSTRAINT [DF_tbl_Country_CreatedOn] DEFAULT (getdate()) NOT NULL,
+    [CreatedBy]  INT            NULL,
+    [ModifiedOn] DATETIME       NULL,
+    [ModifiedBy] INT            NULL,
+    [RowVersion] ROWVERSION     NULL,
+    CONSTRAINT [PK_tbl_Country] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_tbl_Country_tbl_Tenant] FOREIGN KEY ([TenantId]) REFERENCES [dbo].[tbl_Tenant] ([TenantId])
 );
+
+
 
 
 GO
--- Database/SatvaPartyRegister.Database/dbo/Tables/tbl_State.sql
+++ Database/SatvaPartyRegister.Database/dbo/Tables/tbl_State.sql
@@ -1,18 +1,22 @@
 ﻿CREATE TABLE [dbo].[tbl_State] (
-    [Id]         INT           NOT NULL,
-    [Code]       NVARCHAR (50) NOT NULL,
-    [CountryId]  INT           NOT NULL,
-    [StateName]  VARCHAR (50)  NOT NULL,
-    [ShortCode]  VARCHAR (10)  NULL,
-    [StateCode]  VARCHAR (4)   NOT NULL,
-    [CreatedOn]  DATETIME      CONSTRAINT [DF_tbl_State_CreatedOn] DEFAULT (getdate()) NOT NULL,
-    [CreatedBy]  INT           NULL,
-    [ModifiedOn] DATETIME      NULL,
-    [ModifiedBy] INT           NULL,
-    [RowVersion] ROWVERSION    NOT NULL,
+    [Id]         INT            NOT NULL,
+    [Code]       NVARCHAR (50)  NOT NULL,
+    [CountryId]  INT            NOT NULL,
+    [StateName]  VARCHAR (50)   NOT NULL,
+    [TenantId]   NVARCHAR (128) NOT NULL,
+    [ShortCode]  VARCHAR (10)   NULL,
+    [StateCode]  VARCHAR (4)    NOT NULL,
+    [CreatedOn]  DATETIME       CONSTRAINT [DF_tbl_State_CreatedOn] DEFAULT (getdate()) NOT NULL,
+    [CreatedBy]  INT            NULL,
+    [ModifiedOn] DATETIME       NULL,
+    [ModifiedBy] INT            NULL,
+    [RowVersion] ROWVERSION     NOT NULL,
     CONSTRAINT [PK_tbl_State] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_tbl_State_tbl_Country] FOREIGN KEY ([CountryId]) REFERENCES [dbo].[tbl_Country] ([Id])
+    CONSTRAINT [FK_tbl_State_tbl_Country] FOREIGN KEY ([CountryId]) REFERENCES [dbo].[tbl_Country] ([Id]),
+    CONSTRAINT [FK_tbl_State_tbl_Tenant] FOREIGN KEY ([TenantId]) REFERENCES [dbo].[tbl_Tenant] ([TenantId])
 );
+
+
 
 
 GO
